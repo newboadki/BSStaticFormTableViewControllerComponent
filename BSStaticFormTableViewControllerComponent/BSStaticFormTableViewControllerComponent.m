8dd96f51691b2eac12a75a3e56a5872f323cc4c2
Borja Arias Drake <borja.arias@gmail.com>
2014-03-14 07:37:03 +0000

    Initial Commit

---
+++ BSStaticFormTableViewControllerComponent/BSStaticFormTableViewControllerComponent.m
@@ -0,0 +1,13 @@
+//
+//  BSStaticFormTableViewControllerComponent.m
+//  BSStaticFormTableViewControllerComponent
+//
+//  Created by Borja Arias Drake on 14/03/2014.
+//  Copyright (c) 2014 Borja Arias Drake. All rights reserved.
+//
+
+#import "BSStaticFormTableViewControllerComponent.h"
+
+@implementation BSStaticFormTableViewControllerComponent
+
+@end
---
+++ BSStaticFormTableViewControllerComponentTests/BSStaticFormTableViewControllerComponentTests.m
@@ -0,0 +1,34 @@
+//
+//  BSStaticFormTableViewControllerComponentTests.m
+//  BSStaticFormTableViewControllerComponentTests
+//
+//  Created by Borja Arias Drake on 14/03/2014.
+//  Copyright (c) 2014 Borja Arias Drake. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BSStaticFormTableViewControllerComponentTests : XCTestCase
+
+@end
+
+@implementation BSStaticFormTableViewControllerComponentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
